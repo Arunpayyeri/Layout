c449196992ce6c068dcf028f47e8aa7cef7ed974
arunpayyeri <arunpayyeri@gmail.com>
2018-04-23 11:31:00 +0530

    FIrst commit

---
+++ app/src/main/java/com/example/arun/lay/NextActivity.java
@@ -0,0 +1,18 @@
+package com.example.arun.lay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+
+
+        }
+
+}
